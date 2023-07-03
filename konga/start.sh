94412df34e29db479d289b483fd14f80537f02b7
francisco <elcofla46@hotmail.com>
2023-07-02 21:27:10 -0500

    trying start.sh

--- konga/start.sh
+++ konga/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-POSTGRESQL_HOST=$1
+# POSTGRESQL_HOST=$1
 
-# Wait for PostgreSQL to be ready
-until pg_isready -h $POSTGRESQL_HOST -p 5432 -U $POSTGRESQL_USER; do
-  echo "Waiting for PostgreSQL to be available..."
-  sleep 1
-done
+# # Wait for PostgreSQL to be ready
+# until pg_isready -h $POSTGRESQL_HOST -p 5432 -U $POSTGRESQL_USER; do
+#   echo "Waiting for PostgreSQL to be available..."
+#   sleep 1
+# done
 
 # Run Konga
 exec npm start
